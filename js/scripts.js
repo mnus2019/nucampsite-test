6fd50c3d9f0162ef4e890b7221615830ad2851f2
mnus2019 <tontoas2018@gmail.com>
2020-06-01 22:41:37 -0800

    NPM Scripts Part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
-//  $(function() {
-//             $(".carousel").carousel( { interval: 2000 } );
-//             $("#carouselButton").click(function(){
-//                 if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-//                     $(".carousel").carousel("pause");
-//                     $("#carouselButton").children("i").removeClass("fa-pause");
-//                     $("#carouselButton").children("i").addClass("fa-play");
-//                 } else {
-//                     $(".carousel").carousel("cycle");
-//                     $("#carouselButton").children("i").removeClass("fa-play");
-//                     $("#carouselButton").children("i").addClass("fa-pause"); 
-//                 }
-                
-//             });
-//         });
-        
+$(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").mouseover(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause"); 
+        }
+    });
+});
